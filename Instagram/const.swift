4ffe81c57d7bd6a6b25da6e9a67496ff05ecca41
kazuki.kawamura <k09046607339@yahoo.co.jp>
2021-02-14 10:29:05 +0900

    //カリキュラム作成

---
+++ Instagram/const.swift
@@ -0,0 +1,35 @@
+//
+//  const.swift
+//  Instagram
+//
+//  Created by kazuki on 2021/02/11.
+//
+
+import UIKit
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
+
+class const: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
